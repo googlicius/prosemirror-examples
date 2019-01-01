29b4f6d9b93c2dd1b715b3f0d0d691aaef29a0c7
Googlius <googlicius@gmail.com>
2019-01-01 23:12:39 +0700

    KJKJJK

--- src/plugins/sync-name-attribute.js
+++ src/plugins/sync-name-attribute.js
@@ -5,32 +5,34 @@ import { makeid } from "./../pure-func";
 export default function syncNameAttribute() {
     return new Plugin({
         view: editorView => {
+            const state = editorView.state;
+
             /**
              * Set name attribute to `paragraph`, `heading`.
              */
             function setName() {
-                const state = editorView.state;
+                let tr = state.tr;
                 state.doc.forEach((node, offset) => {
                     switch (node.type.name) {
                         case 'paragraph':
                             if (node.attrs.name == null) {
-                                editorView.dispatch(state.tr.setBlockType(
-                                    offset + 1, 
-                                    offset + node.nodeSize, 
+                                tr = tr.setBlockType(
+                                    offset + 1,
+                                    offset + node.nodeSize,
                                     state.schema.nodes.paragraph, 
                                     { name: makeid() }
-                                ));
+                                );
                             }
                             break;
                             
                         case 'heading':
                             if (node.attrs.name == null) {
-                                editorView.dispatch(state.tr.setBlockType(
+                                tr = tr.setBlockType(
                                     offset + 1, 
                                     offset + node.nodeSize, 
                                     state.schema.nodes.heading, 
                                     { level: node.attrs.level, name: makeid() }
-                                ));
+                                );
                             }
                             break;
 
@@ -40,12 +42,12 @@ export default function syncNameAttribute() {
                                 item_node.forEach((paragraph_node, offset3) => {
                                     if (paragraph_node.attrs.name == null) {
                                         const total_offset = offset + offset2 + offset3 + 2;
-                                        editorView.dispatch(state.tr.setBlockType(
+                                        tr = tr.setBlockType(
                                             total_offset + 1, 
                                             total_offset + paragraph_node.nodeSize, 
                                             state.schema.nodes.paragraph, 
                                             { name: makeid() }
-                                        ));
+                                        );
                                     }
                                 });
                             })
@@ -55,17 +57,18 @@ export default function syncNameAttribute() {
                             node.forEach((paragraph_node, offset2) => {
                                 if (paragraph_node.attrs.name == null) {
                                     const total_offset = offset + offset2 + 1;
-                                    editorView.dispatch(state.tr.setBlockType(
+                                    tr = tr.setBlockType(
                                         total_offset + 1, 
                                         total_offset + paragraph_node.nodeSize, 
                                         state.schema.nodes.paragraph, 
                                         { name: makeid() }
-                                    ));
+                                    );
                                 }
                             });
                             break;
                     }
                 });
+                editorView.dispatch(tr);
             }
 
             (function init() {
